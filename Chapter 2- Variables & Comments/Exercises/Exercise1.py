64a05fed1edb4b9d33b7ee9cba99a3f05643c28c
Klaithem <113086951+Klaithem1@users.noreply.github.com>
2022-10-26 17:46:54 +0400

    Chapter 2_Exercises

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise1.py
@@ -0,0 +1,6 @@
+## Exercise 1: Variables:
+
+name1 = "Van"
+name2 = "nes"
+name3 = "sa"
+print(name1 + name2 + name3)
---
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -0,0 +1,5 @@
+## Exercise 2: Variables:
+
+author = "Ellie Chu"
+quote =  "\"Love isn't patient and kind and humble. Love is messy and horrible and selfish and bold. It's not finding your perfect half.\""
+print(author + " once said, " + quote)
