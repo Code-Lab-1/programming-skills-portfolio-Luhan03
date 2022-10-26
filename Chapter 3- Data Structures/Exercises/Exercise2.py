84c5c3281f5c69659dd2e3722c0c4ee5233f038b
Klaithem <113086951+Klaithem1@users.noreply.github.com>
2022-10-26 18:02:19 +0400

    Chapter 3_Finished Exercises

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,21 @@
+## Exercise 2: Greetings
+
+names = ["Rossele", "Pauline","Eroll","Arjay","Dan", "Von"]
+
+msg = "\nHow are you, " + names[0] + "?"
+print(msg)
+
+msg = "How are you, " + names[1] + "?"
+print(msg)
+
+msg = "How are you," + names[2] + "?"
+print(msg)
+
+msg = "How are you," + names[3] + "?"
+print(msg)
+
+msg = "How are you," + names[4] + "?"
+print(msg)
+
+msg = "How are you," + names[5] + "?"
+print(msg)
---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,21 @@
+## Exercise 4: Guest List
+
+names = ["Rossele", "Pauline","Eroll","Arjay","Dan", "Von"]
+
+name = names[0]
+print(name + ", Join me to dinner.")
+
+name = names[1]
+print(name + ", Join me to dinner.")
+
+name = names[2]
+print(name + ", Join me to dinner.")
+
+name = names[3]
+print(name + ", Join me to dinner.")
+
+name = names[4]
+print(name + ", Join me to dinner.")
+
+name = names[5]
+print(name + ", Join me to dinner.")
